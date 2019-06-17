10365f5b9228574d0b87929922dfc295db85367f
christopher bell <christopherbell@christophersAir.fios-router.home>
2019-06-17 10:57:30 -0400

    server is setup and running / added some html to test with

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var handlebars = require("express-handlebars");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
-var logger = require("morgan");
+// var logger = require("morgan");
 
 // Require all models
 var db = require("./models");
@@ -16,7 +16,7 @@ var app = express();
 // Configure middleware
 
 // Use morgan logger for logging requests
-app.use(logger("dev"));
+// app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,10 +28,16 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 
 // 7. When you go to connect your mongo database to mongoose, do so the following way:
 
-```js
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// ```js
+// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
-```
+// mongoose.connect(MONGODB_URI);
+// ```
 // * This code should connect mongoose to your remote mongolab database if deployed, but otherwise will connect to the local mongoHeadlines database on your computer.
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
