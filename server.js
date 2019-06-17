0867a7b798407137c46f353a30bac2ec81752f6e
christopher bell <christopherbell@christophersAir.fios-router.home>
2019-06-17 10:38:46 -0400

    added some server code, setup heroku and installed addons

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var handlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var logger = require("morgan");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+// 7. When you go to connect your mongo database to mongoose, do so the following way:
+
+```js
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+```
+// * This code should connect mongoose to your remote mongolab database if deployed, but otherwise will connect to the local mongoHeadlines database on your computer.
