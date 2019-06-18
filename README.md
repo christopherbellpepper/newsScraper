eaf88ac77e38f7eaa1707f44cfac349ccaaddddf
christopher bell <christopherbell@christophers-MacBook-Air.local>
2019-06-17 20:25:12 -0400

    adjusted html

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # newsScraper
+
+https://bell-pepper-news-scraper.herokuapp.com/
+
 flex your Mongoose and Cheerio muscles to scrape news from another site.
--- public/app.js
+++ public/app.js
@@ -7,7 +7,6 @@ $.getJSON("/articles", function(data) {
     }
   });
   
-  
   // Whenever someone clicks a p tag
   $(document).on("click", "p", function() {
     // Empty the notes from the note section
